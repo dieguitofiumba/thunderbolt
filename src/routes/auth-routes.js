a1440e7a29ac8dce5703c8b8c313c1cb8f2dbf1e
DieguitoFiumba <diegoandresv83@gmail.com>
2024-09-07 01:12:41 -0400

    2.29

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const discordAuth = require('../controllers/authControllers')
 
 router.get('/auth/discord', discordAuth)
-router.post('/auth/token', tokenAuth)
+//router.post('/auth/token', tokenAuth)
 
 module.exports = router
